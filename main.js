738b735a7f630cf1ca75c56eaf6c32acd6a765d8
dhanush-danny <78251450+dhanush-danny@users.noreply.github.com>
2022-01-13 19:01:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function home(){
+    window.location.href = "https://dhanush-danny.github.io/code-away-1/";
+}
+
+function clicked_css(){
+    document.getElementById("p_1").innerHTML="CSS Introduction";
+    document.getElementById("p_2").innerHTML="CSS - The language we use to style a web page.";
+    document.getElementById("p_3").innerHTML="What is CSS?";
+    document.getElementById("p_4").innerHTML="*  CSS stands for Cascading Style Sheets.";
+    document.getElementById("p_5").innerHTML="*  CSS describes how HTML elements are to be displayed on screen, paper, or in other media.";
+    document.getElementById("p_6").innerHTML="*  CSS controls the layout of multiple web pages all at once.";
+    document.getElementById("p_7").innerHTML="*  There are three types of CSS files :- Inline CSS, Internal or Embedded CSS, External CSS.";
+    document.getElementById("p_8").innerHTML="*  It can be used to adjust content size, spacing, color and font or add decorative features, such as animations or split content into columns.";
+    document.getElementById("p_9").innerHTML="CSS demo, 1 CSS page, 1 HTML page.";
+    document.getElementById("p_10").innerHTML="body {"+"<br>"+"background-color: green;"+"<br>"+"}"+"<br><br>"+"p {"+"<br>"+"background-color: red;"+"<br>"+"font-family: Verdana;"+"<br>"+"}"+"<br><br>"+"img {"+"<br>"+"width: 500px;"+"<br>"+"height: 500px;"+"<br>"+"}";
+}
